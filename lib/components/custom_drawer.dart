c2a94c2a42f9cbd9902404b496e99a03b714ca5d
ahmed adel <ahmedadel123422@gmail.com>
2025-01-15 17:16:19 +0200

    commit

--- lib/components/custom_drawer.dart
+++ lib/components/custom_drawer.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -8,83 +6,109 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return //* Add a Drawer
-        Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          //* Drawer Header
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.grey,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    // Get the current user
+    final User? user = FirebaseAuth.instance.currentUser;
+    final String userEmail = user?.email ?? "user@example.com";
+
+    return Drawer(
+      child: Padding(
+        padding: EdgeInsets.only(bottom: 25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
               children: [
-                CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.white,
-                  child: Icon(
-                    Icons.person,
-                    size: 40,
+                //* Drawer Header
+                DrawerHeader(
+                  decoration: BoxDecoration(
                     color: Colors.grey,
                   ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Welcome!",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                  child: Container(
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.person,
+                            size: 40,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        const Text(
+                          "W E L C O M E",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          userEmail, // Display the user's email
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                Text(
-                  "user@example.com", // Replace with the user's email if available
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 14,
+                //* Drawer Items
+                ListTile(
+                  leading: const Icon(
+                    Icons.home,
+                    color: Colors.grey,
                   ),
+                  title: const Text("H O M E"),
+                  onTap: () {
+                    // Navigate to the home screen
+
+                    Navigator.pop(context); // Close the drawer
+                  },
                 ),
+                ListTile(
+                  leading: const Icon(
+                    Icons.settings,
+                    color: Colors.grey,
+                  ),
+                  title: const Text("P R O F I L E"),
+                  onTap: () {
+                    // Navigate to the settings screen
+                    Navigator.pop(context); // Close the drawer
+                  },
+                ),
+                ListTile(
+                  leading: const Icon(
+                    Icons.help,
+                    color: Colors.grey,
+                  ),
+                  title: const Text("U S E R S"),
+                  onTap: () {
+                    // Navigate to the help screen
+                    Navigator.pop(context); // Close the drawer
+                  },
+                ),
+                const Divider(), // Add a divider
               ],
             ),
-          ),
-          //* Drawer Items
-          ListTile(
-            leading: Icon(Icons.home),
-            title: Text("Home"),
-            onTap: () {
-              // Navigate to the home screen
-              Navigator.pop(context); // Close the drawer
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text("Settings"),
-            onTap: () {
-              // Navigate to the settings screen
-              Navigator.pop(context); // Close the drawer
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.help),
-            title: Text("Help"),
-            onTap: () {
-              // Navigate to the help screen
-              Navigator.pop(context); // Close the drawer
-            },
-          ),
-          Divider(), // Add a divider
-          ListTile(
-            leading: Icon(Icons.logout),
-            title: Text("Logout"),
-            onTap: () {
-              // Sign out the user
-              FirebaseAuth.instance.signOut();
-              Navigator.pop(context); // Close the drawer
-            },
-          ),
-        ],
+            ListTile(
+              leading: const Icon(
+                Icons.logout,
+                color: Colors.grey,
+              ),
+              title: const Text("L O G O U T"),
+              onTap: () {
+                // Sign out the user
+                FirebaseAuth.instance.signOut();
+                Navigator.pop(context); // Close the drawer
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:minimal_social/auth/auth.dart';
+import 'package:minimal_social/pages/HomePage.dart';
+import 'package:minimal_social/pages/profile_page.dart';
+import 'package:minimal_social/pages/register_page.dart';
+import 'package:minimal_social/pages/user_page.dart';
 import 'package:minimal_social/theme/dark.dart';
 import 'package:minimal_social/theme/light.dart';
 
@@ -19,6 +23,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      routes: {
+        '/auth': (context) => AuthPage(),
+        '/profile': (context) => ProfilePage(),
+        '/homepage': (context) => HomePage(),
+        '/users': (context) => UserPage(),
+      },
       home: AuthPage(),
       theme: lightMode,
       darkTheme: darkMode,
