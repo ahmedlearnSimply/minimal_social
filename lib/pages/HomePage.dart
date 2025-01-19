6761fe41b94bf06230dcddced23d03a192c291ba
ahmed adel <ahmedadel123422@gmail.com>
2025-01-19 19:44:37 +0200

    commit

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:minimal_social/components/custom_drawer.dart';
 
